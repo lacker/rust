d80d4f99e48ce1f1dc1855c946f97b2f7114deb4
Kevin Lacker <lacker@gmail.com>
2016-01-17 21:09:59 -0800

    235 problem in rust

--- main.rs
+++ main.rs
@@ -1,3 +1,23 @@
+use std::collections::BinaryHeap;
+
+// Prints out numbers whose only prime factors are 2, 3, and 5.
 fn main() {
-   println!("yo wassup world!");
+    let mut heap = BinaryHeap::new();
+    heap.push(-1);
+
+    let mut last = 0;
+    loop {
+        let i = heap.pop();
+        let x = match i {
+            Some(n) => -n,
+            None => 0,
+        };
+        if x > last {
+            println!("{}", x);
+            last = x;
+            heap.push(-2 * x);
+            heap.push(-3 * x);
+            heap.push(-5 * x);
+        }
+    }
 }
